bd95b1a7247f11dcadbae2eeba2f756fca21332e
yewei <yewei@nibirutech.com>
2020-09-04 10:47:35 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+
+export const Home = (): JSX.Element => (
+  <div className="container">
+    <Head>
+      <title>啊我死了.啦</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1 className="title">AWSL.LA</h1>
+    </main>
+
+    <footer>
+      <div>啊我死了.啦</div>
+    </footer>
+
+    <style jsx>{`
+      .container {
+        min-height: 100vh;
+        padding: 0 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid #eaeaea;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .title {
+        margin: 0;
+        line-height: 1.15;
+        font-size: 4rem;
+        letter-spacing: 2rem;
+      }
+
+      .title {
+        text-align: center;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        color: #6c6c6c;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
